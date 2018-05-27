c49a824c2ae2bae6e8afa789ede6d00c7d1babeb
Vladimír Dušek <vladik.dusek@seznam.cz>
2018-05-27 15:11:56 +0200

    úprava

--- SQLconnection/src/aplikace/Main.java
+++ SQLconnection/src/aplikace/Main.java
@@ -9,6 +9,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
